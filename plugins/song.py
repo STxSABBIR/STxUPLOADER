5efc7f450e56080968b700dcfb82e21c6b89857b
Muhammad Sabbir <138793371+STxSABBIR@users.noreply.github.com>
2023-08-12 22:06:39 +0600

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -18,8 +18,8 @@ from pyrogram.types import Message
 from youtube_search import YoutubeSearch
 from youtubesearchpython import SearchVideos
 
-from VCPlayBot.config import DURATION_LIMIT
-from VCPlayBot.modules.play import arq
+from config import DURATION_LIMIT
+from stxuploader import arq
 
 
 @Client.on_message(filters.command("song") & ~filters.channel)
