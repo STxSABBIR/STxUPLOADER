f58173354221e60b8e74560bcf4f924ef90a6dac
TheFunkyFox <139276257+TheFunkyFox@users.noreply.github.com>
2023-08-12 22:18:43 +0600

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -17,6 +17,7 @@ from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.types import Message
 from youtube_search import YoutubeSearch
 from yt_dlp import YoutubeDL
+from config import Config
 
 from config import BOT_USERNAME as bn
 
