dc20a792c201bf2a04ecb5c0f63be7da1b18dae5
Muhammad Sabbir <138793371+STxSABBIR@users.noreply.github.com>
2023-08-13 12:13:08 +0600

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -15,7 +15,6 @@ if bool(os.environ.get("WEBHOOK", False)):
     from config import Config
 else:
     from config import Config
-from config import BOT_USERNAME as stx
 
 # the Strings used for this "thing"
 from translation import Translation
@@ -23,7 +22,7 @@ from translation import Translation
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 from pyrogram import filters 
-from pyrogram import Client as stx
+from pyrogram import Client as Mai_bOTs
 
 #from helper_funcs.chat_base import TRChatBase
 from helper_funcs.display_progress import progress_for_pyrogram
